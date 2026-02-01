781df792b1e58169df95287c26c5a8480e3585cc
torrejoNia <tefi1805@hotmail.com>
2026-02-01 16:05:05 +0000

    sunday1

---
+++ libft1.c
@@ -0,0 +1,83 @@
+
+#include "push_swap.h"
+
+long	ft_atoi(const char *str)
+{
+	long	i;
+	long	number;
+	int		sign;
+
+	i = 0;
+	number = 0;
+	sign = 1;
+	while (str[i] && (str[i] == 32 || (str[i] >= 9 && str[i] <= 13)))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		number = (number * 10) + (str[i] - '0');
+		i++;
+	}
+	return (number * sign);
+}
+
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned const char	*pt_src;
+	unsigned char		*pt_dest;
+	size_t				i;
+
+	pt_dest = (unsigned char *)dest;
+	pt_src = (unsigned char *)src;
+	i = 0;
+	if (dest == NULL && src == NULL)
+		return (NULL);
+	while (i < n)
+	{
+		*((char *)dest + i) = *((char *)pt_src + i);
+		i++;
+	}
+	return (pt_dest);
+}
+
+static int	ft_count_words(char const *str, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == c)
+			i++;
+		else
+		{
+			count++;
+			while (str[i] && str[i] != c)
+				i++;
+		}
+	}
+	return (count);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != (const char)c && *s)
+		s++;
+	if (*s == (char)c)
+		return ((char *)s);
+	return (NULL);
+}
---
+++ libft2.c
@@ -0,0 +1,67 @@
+
+#include "push_swap.h"
+
+	/*
+	Returns a new string that is a duplicate of the input string.
+	It allocates memory for the new string and copies the contents
+	of the input string into the new memory location.
+	*/
+
+char	*ft_strdup(const char *s)
+{
+	size_t	len;
+	char	*s_alloc;
+
+	len = ft_strlen(s) + 1;
+	s_alloc = (char *)malloc(len * sizeof(char));
+	if (s_alloc == NULL)
+		return (NULL);
+	ft_memcpy(s_alloc, s, len);
+	free (s_alloc);
+	return (s_alloc);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (*str != '\0')
+	{
+		str++;
+		i++;
+	}
+	return (i);
+}
+
+	/*
+	Creates a new string that is a portion of an input string,
+	starting from a specified index and with a specified length.
+	*/
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	count;
+	size_t	size;
+	char	*tab;
+
+	if (!s)
+		return (NULL);
+	if ((unsigned int)ft_strlen(s) < start)
+		return (ft_strdup(""));
+	size = ft_strlen(s + start);
+	if (size < len)
+		len = size;
+	tab = (char *)malloc((len + 1) * sizeof(char));
+	if (!tab)
+		return (NULL);
+	count = 0;
+	while (count < len)
+	{
+		tab[count] = s[start + count];
+		count++;
+	}
+	tab[count] = '\0';
+	free (tab);
+	return (tab);
+}
